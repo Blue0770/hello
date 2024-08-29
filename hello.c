6a4a87e9d3c6d5d971f2c1984062da4e603836a1
Blue0770 <golsen8@huskers.unl.edu>
2024-08-29 13:06:25 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Gavin Olsen
+ *         golsen8@unl.edu
+ * Date: 2024/8/29
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Gavin Olsen\n");
+  return 0;
+  
+}
